01d7263bb06af3607ec8a6a22fb7611026da2841
Igor Shevtsov <infoshevtsov@gmail.com>
2024-12-28 16:34:54 +0000

    completed hello world

---
+++ hello/v6/hello.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const (
+	spanish            = "Spanish"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
