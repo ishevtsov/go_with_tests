fc6880c350deafc15594eef4836d88a9ed0c6e7e
Igor Shevtsov <infoshevtsov@gmail.com>
2025-01-25 17:34:36 +0000

    updated iteration with Builder optimization

--- iteration/v3/repeat.go
+++ iteration/v3/repeat.go
@@ -1,13 +1,11 @@
 package iteration
 
-import "strings"
-
 const repeatCount = 5
 
 func Repeat(character string) string {
-	var repeated strings.Builder
+	var repeated string
 	for i := 0; i < repeatCount; i++ {
-		repeated.WriteString(character)
+		repeated += character
 	}
-	return repeated.String()
+	return repeated
 }
